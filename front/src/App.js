dbc7225b5bc7a377bfda6cf5b4d2b52884f4e80d
Becker <yuri.becker@dbccompany.com.br>
2020-04-02 21:39:52 -0300

    feat(Header): adicionar componente header.

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Card from './Components/Card/Card'
+import Header from './Components/Header/Header'
 export default function App() {
   return (
     <div>
-        <Card name="Teste" image="https://static.wixstatic.com/media/0adf28_eec83b33056243a9a33440a10b5d7afd.png"/>
+        <Header />
     </div>
   );
 }
---
+++ front/src/Components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./header.css"
+
+export default () => 
+    <div className="header-container">
+        <span>Startups</span>
+        <span>Resultados</span>
+    </div>
