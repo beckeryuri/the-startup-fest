ee83679fc5da555a2b379f1bd63fff794c0d8b4c
Becker <yuri.becker@dbccompany.com.br>
2020-04-03 12:11:55 -0300

    fix(PopUp): altear popup para um tamanho fixo e criar uma barra de rolagem para as informacoes adicionais

--- front/src/Components/Popup/PopUp.jsx
+++ front/src/Components/Popup/PopUp.jsx
@@ -7,6 +7,7 @@ export default (props) =>
         <img className="popup-img" src={"https://static.wixstatic.com/media/0adf28_eec83b33056243a9a33440a10b5d7afd.png"} alt="logo"/>
         <h1>Nome da Startup</h1>
         <h3>Teste Testando</h3>
+        <p>Plataforma que oferece ao micro e pequeno empreendedor uma inteligência de negócios de forma intuitiva e eficiente através do entendimento e controle dos números da empresa.</p>
 
         <StarRating name={"Apresentação"}/>
         <StarRating name={"Proposta / Pitch"}/>
