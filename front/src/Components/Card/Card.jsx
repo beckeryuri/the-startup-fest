ea915a031212a54fb7315efc9df2cf9989eb1234
Becker <yuri.becker@dbccompany.com.br>
2020-04-04 18:02:38 -0300

    wip(LayoutAlterantivo): criar layout com cards maiores e que em telas menores crie um popup paa a votacao e leitura da proposta da statup.

--- front/src/Components/Card/Card.jsx
+++ front/src/Components/Card/Card.jsx
@@ -1,19 +1,20 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import "./card.css"
 import PopUp from '../Popup/PopUp'
+import StarRating from '../StarRating/StarRating'
 
-export default class Card extends Component{
-    constructor(props){
+export default class Card extends Component {
+    constructor(props) {
         super(props)
         this.state = { hidden: false }
     }
 
-    openPopUp(){
+    openPopUp() {
         this.setState({ hidden: true })
     }
 
-    closePopUp(){
-        this.setState({hidden: false})
+    closePopUp() {
+        this.setState({ hidden: false })
         console.log("Foi FOOOOOOOI")
 
         console.log(this.state.hidden)
@@ -23,32 +24,43 @@ export default class Card extends Component{
         const { hidden } = this.state
         const { image, name, description } = this.props
 
-		return (
-			<React.Fragment>
+        return (
+            <React.Fragment>
 
-				{hidden && (
+                {hidden && (
 
-                    <PopUp onClick={this.closePopUp.bind(this)} name={name} image={image} description={description}/>
-				    )
-				}
-			</React.Fragment>
-		)
-	}
+                    <PopUp onClick={this.closePopUp.bind(this)} name={name} image={image} description={description} />
+                )
+                }
+            </React.Fragment>
+        )
+    }
 
-    render(){
-        const {image, name} = this.props
-        return(
+    render() {
+        const { image, name, description } = this.props
+        return (
             <React.Fragment>
                 {this.showPopUp()}
-                <div onClick={this.openPopUp.bind(this)} className="card-container">
-                    <img src={image} alt="logo"/>
-                    <h1>{name}</h1>
+                <div className="card-container">
+                    <div className="logo-container">
+                        <img src={image} alt="logo" />
+                        <h1>{name}</h1>
+                        <div onClick={this.openPopUp.bind(this)}className="popup-button"></div>
+                    </div>
+                    <div className="description-container">
+                        <p>{description}</p>
+                    </div>
+                        <div className="stars-container">
+                            <StarRating name={"Apresentação"} />
+                            <StarRating name={"Proposta / Pitch"} />
+                            <StarRating name={"Desenvolvimento"} />
+                            <button>Votar!</button>
+                        </div>
                 </div>
-                
+
             </React.Fragment>
         )
     }
 }
 
 
-        
