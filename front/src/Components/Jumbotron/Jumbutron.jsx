df9149958efe9a5a78e23668a054d3acb55d3274
Becker <yuri.becker@dbccompany.com.br>
2020-04-06 16:17:26 -0300

    fix(Resultados): alterar layout de resultados para deixar tela responsiva em telas menores.

--- front/src/Components/Jumbotron/Jumbutron.jsx
+++ front/src/Components/Jumbotron/Jumbutron.jsx
@@ -4,10 +4,10 @@ import StarRating from '../StarRating/StarRating'
 
 export default (props) => 
         <div className="jumbutron-container" >
-            <h1>1º</h1>
+            <h1>{props.ranking}</h1>
              <img className="jumbutron-logo" src="https://static.wixstatic.com/media/0adf28_eec83b33056243a9a33440a10b5d7afd.png" alt="logo"/>
              <div>
-                 <StarRating name="Nome"/>
-                 <h3>3,5</h3>
+                 <StarRating nota={props.nota} name={props.name}/>
+                <h3>{props.nota}</h3>
              </div>
         </div>
--- front/src/Pages/Ranking/Ranking.jsx
+++ front/src/Pages/Ranking/Ranking.jsx
@@ -13,23 +13,23 @@ export default class Startups extends Component{
       <div>
           <Header />
           <div className="ranking-container">
-          <div>
+          <div className="column-container">
             <h1>Proposta</h1>
-            <Jumbutron />
-            <Jumbutron />
-            <Jumbutron />
+            <Jumbutron ranking={"1º"} nota={2.5} name={"Hellfire Games"}/>
+            <Jumbutron ranking={"2º"} />
+            <Jumbutron ranking={"3º"} />
           </div>
-          <div>
+          <div className="column-container">
             <h1>Apresentação</h1>
-            <Jumbutron />
-            <Jumbutron />
-            <Jumbutron />
+            <Jumbutron ranking={"1º"} nota={2.5} name={"Testando"}/>
+            <Jumbutron ranking={"2º"} />
+            <Jumbutron ranking={"3º"} />
           </div>
-          <div>
+          <div className="column-container">
             <h1>Desenvolvimento</h1>
-            <Jumbutron />
-            <Jumbutron />
-            <Jumbutron />
+            <Jumbutron ranking={"1º"} nota={2.5}/>
+            <Jumbutron ranking={"2º"} />
+            <Jumbutron ranking={"3º"} />
           </div>
         </div>
       </div>
